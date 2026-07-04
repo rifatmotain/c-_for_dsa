3a90d8233c84dd44667755dee829708999fb8a4a
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:05:19 +0600

    Uploaded input.cpp from module_1 separately

---
+++ module_1/input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    int x;
+    char c;
+    double b;
+    cin>>x >>c >>b;
+    int p= c;
+    cout<<(char)x <<endl <<p <<endl <<b;
+    return 0;
+}
