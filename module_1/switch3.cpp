fc8bb38520d5e4c36c03e610550b40cd7d30eab5
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:05:34 +0600

    Uploaded switch3.cpp from module_1 separately

---
+++ module_1/switch3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    char x;
+    cin>> x;
+    switch (x)
+    {
+        case 'a': cout<<"vowel"; break;
+        case 'e': cout<<"vowel"; break;
+        case 'i': cout<<"vowel"; break;
+        case 'o': cout<<"vowel"; break;
+        case 'u': cout<<"vowel"; break;
+        case 'A': cout<<"vowel"; break;
+        case 'E': cout<<"vowel"; break;
+        case 'I': cout<<"vowel"; break;
+        case 'O': cout<<"vowel"; break;
+        case 'U': cout<<"vowel"; break;
+        default : cout<<"consonant";
+    }
+
+}
