349ba27054bec5dee5f57ffb887aaedbcce92c9a
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:21:05 +0600

    Added string from module_5

---
+++ module_5/string.cpp
@@ -0,0 +1,59 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main ()
+// {
+//     char s[10]="hello";
+//     // s="gello"       likhte parbo na
+//     strcpy(s,"gello");
+//     cout<< s<<endl;
+//     return 0;
+// }
+
+//akon amra auvabe direct korbo
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main ()
+// {
+//     string s="hello";
+//     s="gello";
+//     cout<< s<<endl;
+//     return 0;
+// }
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    char s[10]="hello";
+    char s2[10 ]="gello";
+    if (strcmp(s,s)==0)
+    {
+        cout<<"same";
+    }
+    else
+    {
+        cout<<"not same";
+    }
+    cout<< s<<endl;
+    return 0;
+}
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main ()
+// {
+//     string s="hello";
+//     string s2="gello";
+//     if (s==s2)
+//     {
+//         cout<<"same";
+//     }
+//     else
+//     {
+//         cout<<"not same";
+//     }
+//     return 0;
+// }
