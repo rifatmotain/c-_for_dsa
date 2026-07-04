aff5c73e9563a868a9567b759150efc939ab4f1c
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:21:27 +0600

    Added string_constructor from module_6

---
+++ module_6/string_constructor.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    // string s="hello";
+    // cout << s << endl;
+    // string s ("hello");
+    // cout << s << endl;
+    // string s ("hello",2);
+    // cout << s << endl;
+    // string s = "hello world";
+    // string t (s,2);
+    // cout << t << endl;
+    string s (10,'a');
+    cout << s<< endl;
+}
