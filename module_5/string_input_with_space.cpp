5156dbf0219c5ac897894e566c8e43fdc18bd891
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:21:10 +0600

    Added string_input_with_space from module_5

---
+++ module_5/string_input_with_space.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    string s;
+    cin>> x;
+    cin.ignore();
+    
+    getline(cin,s);
+    cout<<x<<" "<<endl<<s;
+    return 0;
+}
