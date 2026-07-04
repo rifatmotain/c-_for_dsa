2da80b6a46528bc5d6f12c83dda850619222d2b5
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:20:29 +0600

    Added dynamic_arraya_size_increase from module_2

---
+++ module_2/dynamic_arraya_size_increase.cpp
@@ -0,0 +1,44 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int a[3];
+//     for (int i=0; i<3; i++)
+//     {
+//         cin >> a[i];
+//     }
+//     int b[5];
+//     for (int i=0; i<5; i++)
+//     {
+//         b[i]=a[i];
+//     }
+//     b[3]=40;
+//     b[4]=50;
+//     for (int i=0; i<5; i++)
+//     {
+//         cout << b[i]<< " ";
+//     }
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int* a= new int [3];
+    for (int i=0; i<3; i++)
+    {
+        cin >> a[i];
+    }
+    int* b= new int [5];
+    for (int i=0; i<5; i++)
+    {
+        b[i]=a[i];
+    }
+    b[3]=40;
+    b[4]=50;
+    delete[] a;
+    for (int i=0; i<5; i++)
+    {
+        cout << b[i]<< " ";
+    }
+}
