96508086f17182aa167eaed9e088c832cc284fcf
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:05:17 +0600

    Uploaded if_else.cpp from module_1 separately

---
+++ module_1/if_else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    int x=19;
+    if (x%2==0)
+    {
+        cout<< "even\n";
+    }
+    else
+    {
+        cout<<"odd\n";
+    }
+    return 0;
+}
