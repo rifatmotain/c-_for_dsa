1ef7cc0795a86233441c194780d7cbcdd36b8737
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:05:21 +0600

    Uploaded min_max_swap.cpp from module_1 separately

---
+++ module_1/min_max_swap.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+// #include<iostream>
+// #include<algorithm>
+int main()
+{
+    using namespace std;
+    int a,b,c;
+    cin >> a>> b >>c;
+    cout << min ({a,b,c});
+
+}
