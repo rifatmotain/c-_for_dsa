493c90281954907b8b4a2c52f15c4d27512c8d83
rifatmotain <rifatmotain@gmail.com>
2026-07-05 01:05:23 +0600

    Uploaded print.cpp from module_1 separately

---
+++ module_1/print.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    int x=100;
+    char c='a';
+    double y=10.266;
+    std:: cout <<x <<"\n" <<c <<std:: endl <<y << std:: endl;
+    std:: cout<< "i love" <<" " << x;
+    return 0;
+}
